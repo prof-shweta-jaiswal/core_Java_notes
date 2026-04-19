0f842352c8198bca4a932a6f2378b6a38f44c244
Shweta Jaiswal <01shwetajais@gmail.com>
2026-04-19 17:08:57 +0530

    Create demo2 class with examples of Java operations
    Added a demo Java class with comments on various operations.

---
+++ lecture2.java
@@ -0,0 +1,55 @@
+class demo2
+{
+    public static void main(String[] args) 
+    {
+        
+    //  Variables/Containers:
+        int a = 14;
+        int b = 10;
+
+
+    //! Operators:
+
+        //*     5               +         5     =     10
+        //?    (Opreand     operator   Opreand       Value)
+
+        // !                  Expression
+//-----------------------------------------------------------------------------------
+    //? 1. Airthemetic Operation
+        // System.out.println(a+b);
+        // System.out.println(a-b);
+        // System.out.println(a/b);
+        // System.out.println(a*b);
+        // System.out.println(a%b);
+
+//----------------------------------------------------------------------------------
+    //? 2. Comaparision Operation
+        // System.out.println(a==b);
+        // System.out.println(a!=b);
+        // System.out.println(a>b);
+        // System.out.println(a<b);
+        // System.out.println(a>=b);
+        // System.out.println(a<=b);
+//----------------------------------------------------------------------------------
+
+    //?3. Assignments Operation:
+        int x = 5;
+        // x += 5;
+        // x -= 5;
+        // x *= 5;
+        // x /= 5;
+        // x %= 5;
+//----------------------------------------------------------------------------------
+        // x++; // PostIncrement
+        // x--; // PostDecrement
+  
+        // ++x; // PreIncrement
+        // --x; // PreDecrement
+        // System.out.println(x);
+ 
+//----------------------------------------------------------------------------------
+    //?4. Logical Operations:
+    // System.out.println(5 > 6 && 5 > 8); // Checks Both Condition is true
+    // System.out.println(53 > 6 || 5 > 8); // Checks Even if One Condition is true
+
+//----------------------------------------------------------------------------------
